cffdf5f6c3caed2eaf25039484efdf1bf5f893d2
hujuran <hujuran@outlook.com>
2021-04-08 15:35:55 +0800

    1

--- projects/hcoco/src/lib/table/co-table.module.ts
+++ projects/hcoco/src/lib/table/co-table.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableTemplateComponent } from './table-template/table-template.component';
 import { TableBaseComponent } from './table-base/table-base.component';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @NgModule({
   declarations: [
@@ -9,9 +10,10 @@ import { TableBaseComponent } from './table-base/table-base.component';
     TableBaseComponent
   ],
   imports: [
+    NzButtonModule,
     CommonModule
   ],
-  exports:[
+  exports: [
     TableTemplateComponent
   ]
 })
