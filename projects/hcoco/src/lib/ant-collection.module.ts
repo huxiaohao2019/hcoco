51ffc36064b40b2e2b0fe835705dc91caa20e8c5
hujuran <hujuran@outlook.com>
2021-04-08 15:47:53 +0800

    1

--- projects/hcoco/src/lib/ant-collection.module.ts
+++ projects/hcoco/src/lib/ant-collection.module.ts
@@ -1,12 +1,90 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
-
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzTreeModule } from 'ng-zorro-antd/tree';
+import { NzPaginationModule } from 'ng-zorro-antd/pagination';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzTimelineModule } from 'ng-zorro-antd/timeline';
+import { NzTabsModule } from 'ng-zorro-antd/tabs';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzResultModule } from 'ng-zorro-antd/result';
+import { NzUploadModule } from 'ng-zorro-antd/upload';
+import { NzRadioModule } from 'ng-zorro-antd/radio';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzTagModule } from 'ng-zorro-antd/tag';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
+import { NzAnchorModule } from 'ng-zorro-antd/anchor';
+import { NzAffixModule } from 'ng-zorro-antd/affix';
+import { NzAlertModule } from 'ng-zorro-antd/alert';
+import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
+import { NzListModule } from 'ng-zorro-antd/list';
+import { NzTreeViewModule } from 'ng-zorro-antd/tree-view';
+import { NzBadgeModule } from 'ng-zorro-antd/badge';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzPopoverModule } from 'ng-zorro-antd/popover';
+import { NzSwitchModule } from 'ng-zorro-antd/switch';
+import { NzTimePickerModule } from 'ng-zorro-antd/time-picker';
+import { NzTreeSelectModule } from 'ng-zorro-antd/tree-select';
+import { NzMessageModule, NzMessageServiceModule } from 'ng-zorro-antd/message';
+import { NzNotificationModule } from 'ng-zorro-antd/notification';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
+  imports: [],
+  exports: [
+    NzAffixModule,
+    NzAlertModule,
+    NzAnchorModule,
+    NzBadgeModule,
+    NzButtonModule,
+    NzCardModule,
+    NzCheckboxModule,
+    NzDatePickerModule,
+    NzDividerModule,
+    NzDropDownModule,
+    NzFormModule,
+    NzGridModule,
+    NzIconModule,
+    NzInputModule,
+    NzInputNumberModule,
+    NzLayoutModule,
+    NzListModule,
+    NzMenuModule,
+    NzMessageModule,
+    NzMessageServiceModule,
+    NzModalModule,
+    NzNotificationModule,
+
+    NzPaginationModule,
+    NzPopoverModule,
+    NzRadioModule,
+    NzResultModule,
+    NzSpinModule,
+    NzSpaceModule,
+    NzSelectModule,
+    NzSwitchModule,
+    NzTableModule,
+    NzTabsModule,
+    NzTagModule,
+    NzTreeModule,
+    NzTreeSelectModule,
+    NzTypographyModule,
+    NzTimelineModule,
+    NzTimePickerModule,
+    NzTreeViewModule,
+    NzUploadModule
   ]
 })
 export class AntCollectionModule { }
--- projects/hcoco/src/lib/table/co-table.module.ts
+++ projects/hcoco/src/lib/table/co-table.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableTemplateComponent } from './table-template/table-template.component';
 import { TableBaseComponent } from './table-base/table-base.component';
-import { NzButtonModule } from 'ng-zorro-antd/button';
+import { AntCollectionModule } from '../ant-collection.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +10,7 @@ import { NzButtonModule } from 'ng-zorro-antd/button';
     TableBaseComponent
   ],
   imports: [
-    NzButtonModule,
+    AntCollectionModule,
     CommonModule
   ],
   exports: [
