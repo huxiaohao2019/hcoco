60b57d9d77fb2f90f3c64c3adffce74d46382e75
hujuran <hujuran@outlook.com>
2021-04-08 16:11:16 +0800

    form template

---
+++ projects/hcoco/src/lib/co-form/co-form.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormTemplateComponent } from './form-template/form-template.component';
+
+
+
+@NgModule({
+  declarations: [FormTemplateComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoFormModule { }
